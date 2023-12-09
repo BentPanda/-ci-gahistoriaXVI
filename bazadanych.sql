4ed3c9501ad2e8c0f032307be97705e4465be024
BentPanda <116880327+BentPanda@users.noreply.github.com>
2023-12-09 15:19:04 +0100

    Add files via upload

---
+++ bazadanych.sql
@@ -0,0 +1,30 @@
+
+Oto cały plik SQL zawierający informacje o świecie w XVI wieku w Twoim pierwotnym formacie bez zmian w nazwach zmiennych:
+
+sql
+Copy code
+CREATE TABLE IF NOT EXISTS tekst (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tresc TEXT
+);
+
+INSERT INTO tekst (tresc) VALUES ('Reformacja i kontrreformacja - Ruchy religijne znacząco wpływające na kulturę i politykę.');
+INSERT INTO tekst (tresc) VALUES ('Odkrycia geograficzne - Europejscy podróżnicy odkrywają nowe lądy i szlaki morskie.');
+INSERT INTO tekst (tresc) VALUES ('Imperium Osmańskie - Apogeum potęgi pod rządami sułtana Sulejmana Wspaniałego.');
+INSERT INTO tekst (tresc) VALUES ('Wojny religijne - Konflikty między katolikami a protestantami, wojny religijne w Niemczech i innych regionach.');
+
+INSERT INTO tekst (tresc) VALUES ('Rozwój demokracji szlacheckiej - Sejm i wolna elekcja, unikalne w skali europejskiej.');
+INSERT INTO tekst (tresc) VALUES ('Rozkwit kultury i nauki - Oświecenie polskie, rozwój drukarstwa, sztuki i nauki.');
+INSERT INTO tekst (tresc) VALUES ('Rozbicie Dzielnicowe - Proces konsolidacji ziem polskich po okresie rozbicia.');
+INSERT INTO tekst (tresc) VALUES ('Powstanie Duchowieństwa Protestanckiego - Rozwój różnych odłamów protestantyzmu w Polsce.');
+INSERT INTO tekst (tresc) VALUES ('Konflikty zewnętrzne - Wojny z Zakonem Krzyżackim, Moskwą, Turcją i Tatarami.');
+INSERT INTO tekst (tresc) VALUES ('Rozwój Parlamentaryzmu - Ukształtowanie się dwuizbowego sejmu (Senat i Izba Poselska).');
+
+INSERT INTO tekst (tresc) VALUES ('Złoty Wiek Polski - Okres prosperity, rozwoju kulturalnego i politycznego.');
+INSERT INTO tekst (tresc) VALUES ('Sytuacja społeczna - Dominacja szlachty, kwestia chłopów i rozwijającego się mieszczaństwa.');
+INSERT INTO tekst (tresc) VALUES ('Stosunki międzynarodowe - Polska jako znaczący gracz w Europie, relacje z sąsiednimi państwami.');
+INSERT INTO tekst (tresc) VALUES ('Wpływ na Europę - Polska jako ważny ośrodek kulturowy i naukowy, wpływający na kierunki rozwoju europejskiego.');
+INSERT INTO tekst (tresc) VALUES ('Unia Lubelska (1569) - Umowa między Królestwem Polskim a Wielkim Księstwem Litewskim, tworząca federację obu państw.');
+INSERT INTO tekst (tresc) VALUES ('Wolna Elekcja - Unikalny w Europie system wyboru króla przez szlachtę bez udziału dziedziczności.');
+INSERT INTO tekst (tresc) VALUES ('Nihil novi (1505) - Ustawa ograniczająca władzę królewską, uznawana za początek "demokracji szlacheckiej".');
+INSERT INTO tekst (tresc) VALUES ('Sarmatyzm - Ideologia i styl życia polskiej szlachty, oparty na mitologizacji i wyidealizowanym obrazie starożytnych Sarmatów.');
